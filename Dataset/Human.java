9efe0616ab53798f23417b89cf18f2bb60aba5cc
TomGeissler <Tom.Geissler@tu-dresden.de>
2013-05-27 14:41:03 +0200

    Datenstruktur + Parser
    Dokumentation kommt heut Abend

---
+++ Dataset/Human.java
@@ -0,0 +1,23 @@
+
+public class Human {
+	
+	String name,gender,home;
+	int age;
+	
+	public Human(String n){
+		name = n;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public String getGender(){
+		return this.gender;
+	}
+	
+	public int getAge(){
+		return this.age;
+	}
+
+}
