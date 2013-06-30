3c5bc442f4c58cd7c5b6576dbe4f77c246820159
TomGeissler <Tom.Geissler@tu-dresden.de>
2013-07-01 00:41:24 +0200

    add and define Controls

--- RadialTimeline/src/Application.java
+++ RadialTimeline/src/Application.java
@@ -1,40 +1,141 @@
 import processing.core.*;
+import controlP5.*;
 
 public class Application extends PApplet {
 
 	PFont interfaceHealines = createFont("../src/typo/OpenSans-Regular.ttf", 18);
 	PFont legendenText = createFont("../src/typo/OpenSans-Light.ttf", 12);
-	PFont normalText = createFont("../src/typo/OpenSans-Light.ttf", 20);
-	PFont Headlines = createFont("../src/typo/OpenSans-Light.ttf", 28);
-	boolean draw = true, s = false;
+	PFont normalText = createFont("../src/typo/OpenSans-Light.ttf", 18);
+	PFont Headlines = createFont("../src/typo/OpenSans-Regular.ttf", 28);
+	ControlP5 cp5;
+	RadioButton dayBtn;
+	CheckBox personCheckb;
+	int myColorBackground = color(0,0,0);
+	boolean redraw = true;
 	Dataset d;
 
 	public void setup(){
 	  size(1000, 700);
 	  background (42);
 	  d = new Dataset("../src/data/data.xml");
-	// The font must be located in the sketch's 
-	// "data" directory to load successfully
+	  cp5 = new ControlP5(this);  
 	  
-	  fill(35);
-	  noStroke();
-	  rect(700, 0, 300, 700);
-		
-	  fill(255);
-	  
-	  textFont(this.interfaceHealines);
-	  text("Visualisierungen", 740, 3*20);
-	  stroke(255);
-	  strokeWeight(1);
-	  line(740,4*20, 740,8*20);
-	  text("Personen", 740, 14*20);
-	  line(740,15*20, 740,19*20);
-	  text("Tag", 740, 24*20);
-	  line(740,25*20, 740,29*20);
+	  this.drawControl();
 	}
 
 	public void draw(){
-		
-
-  }
+		if(redraw){
+			this.drawInterface();
+			textFont(this.Headlines);
+			text("Headline", 40, 2*20);
+			line(40,3*20, 40,6*20);
+			textFont(this.normalText);
+			boolean moreThanOne = false;
+			String sChain = "";
+			for(int i = 0; i< 7; i++){
+				if(moreThanOne && this.dayBtn.getItem(i).getState()){
+					sChain = sChain + ", ";
+					moreThanOne = false;
+				}
+				if(this.dayBtn.getItem(i).getState()){
+				sChain = sChain + this.dayBtn.getItem(i).getName();
+				moreThanOne = true;
+				}
+			}
+			text("Tag: " + sChain,60, 4*20); 
+			
+			moreThanOne = false;
+			sChain = "";
+			for(int i = 0; i< 4; i++){
+				if(moreThanOne && this.personCheckb.getItem(i).getState()){
+					sChain = sChain + ", ";
+					moreThanOne = false;
+				}
+				if(this.personCheckb.getItem(i).getState()){
+				sChain = sChain + this.personCheckb.getItem(i).getName();
+				moreThanOne = true;
+				}
+			}
+			text("Person: " + sChain,60, 5*20); 
+		}
+	}
+	
+	public void drawControl(){
+		    
+		  personCheckb = cp5.addCheckBox("checkBox")
+	              .setPosition(780, 10*20)
+	              .setColorLabel(color(255))
+	              .setSpacingRow(20)
+	              .setSize(20,20)
+		         .setColorForeground(color(100))
+		         .setColorActive(color(255))
+		         .setItemsPerRow(2)
+		         .setColorBackground(color(50))
+		         .setSpacingColumn(60)
+	              .addItem("Albert", 0)
+	              .addItem("Hannes", 50)
+	              .addItem("Sophia", 100)
+	              .addItem("Tom", 150);
+		  
+		  dayBtn = cp5.addRadioButton("radioButton")
+		         .setPosition(780,17*20)
+		         .setSize(20,20)
+		         .setSpacingRow(20)
+		         .setColorForeground(color(100))
+		         .setColorActive(color(255))
+		         .setItemsPerRow(2)
+		         .setColorBackground(color(50))
+		         .setSpacingColumn(60)
+		         .addItem("Montag",1)
+		         .addItem("Dienstag",2)
+		         .addItem("Mittwoch",3)
+		         .addItem("Donnerstag",4)
+		         .addItem("Freitag",5)
+		         .addItem("Samstag",6)
+		         .addItem("Sonntag",7);
+		     
+		  for(Toggle t:dayBtn.getItems()) {
+		       t.captionLabel().setColorBackground(color(20));
+		       t.setColorCaptionLabel(color(255));
+		       t.captionLabel().style().moveMargin(0,0,0,0);
+		       t.captionLabel().style().movePadding(0,0,0,0);
+		       t.captionLabel().style().backgroundWidth = 0;
+		       t.captionLabel().style().backgroundHeight = 0;
+		  }
+		    this.dayBtn.activate(0); 
+		    this.personCheckb.activate(0);
+	}
+	
+	public void drawInterface(){
+		background (42);
+		  fill(35);
+		  noStroke();
+		  rect(700, 0, 300, 700);	
+		  fill(255);
+		  
+			textFont(this.interfaceHealines);
+			  text("Visualisierungen", 760, 2*20);
+			  stroke(255);
+			  strokeWeight(1);
+			  line(760,3*20, 760,7*20);
+			  text("Personen", 760, 9*20);
+			  line(760,10*20, 760,14*20);
+			  text("Tag", 760, 16*20);
+			  line(760,17*20, 760,25*20);
+			  text("Style", 760, 27*20);
+			  line(760,28*20, 760,32*20);  
+		    this.redraw = false;
+	}
+	
+	public void radioButton(int a) {
+		if(a >= 0){
+		  this.redraw = true;
+		}
+	}
+	
+	public void checkBox(float[] a) {
+		  println(a);
+		  this.redraw = true;
+		}
+	
 }
