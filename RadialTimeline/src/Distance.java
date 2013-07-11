7db30c74b7a232808188f10784aba030b496bcff
TomGeissler <Tom.Geissler@tu-dresden.de>
2013-07-11 13:10:33 +0200

    kaputten scheiss pulln...

--- RadialTimeline/src/Distance.java
+++ RadialTimeline/src/Distance.java
@@ -30,26 +30,27 @@ public class Distance {
 
 				if ((aAct[j].getBeginTime()[0] >= (i - 1) * 2)
 						&& (aAct[j].getBeginTime()[0] < i * 2)) {
-					if ((i - 1) < 4)
-						this.distance[(i - 1) + 8] = this.distance[(i - 1) + 8]
+					activityCount++;
+					if ((i - 1) < 3)
+						this.distance[i+ 8] = this.distance[i + 8]
 								+ aAct[j].distance;
 					else
-						this.distance[(i - 1) - 4] = this.distance[(i - 1) - 4]
+						this.distance[i - 4] = this.distance[i - 4]
 								+ aAct[j].distance;
 				}
 			}
 			if (activityCount == 0) {
-				if ((i - 1) < 4)
-					this.distance[(i - 1) + 8] = 0;
+				if ((i - 1) < 3)
+					this.distance[i + 8] = 0;
 				else
-					this.distance[(i - 1) - 4] = 0;
+					this.distance[i - 4] = 0;
 			} else {
-				if ((i - 1) < 4)
-					this.distance[(i - 1) + 8] = this.distance[(i - 1) + 8]
-							/ activityCount;
+				if ((i - 1) < 3)
+					this.distance[i + 8] = (int)(this.distance[i + 8]
+							/ activityCount);
 				else
-					this.distance[(i - 1) - 4] = this.distance[(i - 1) - 4]
-							/ activityCount;
+					this.distance[i - 4] = (int)(this.distance[i - 4]
+							/ activityCount);
 			}
 		}
 
@@ -67,12 +68,14 @@ public class Distance {
 		p.fill(70);
 		p.ellipseMode(p.CENTER);
 
+		if(this.highest != 0){
 		for (int i = 0; i < 12; i++) {
 			float klaus = (this.distance[i] * 420) / this.highest + 160;
 			float diameter = (float) (p.min(p.height, klaus / dia.scale));
 			p.arc(x, y, diameter, diameter, lastAngle,
 					lastAngle + p.radians(angle));
 			lastAngle += p.radians(angle);
+		}
 		}
 
 	}
