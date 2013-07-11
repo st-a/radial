0362970b2019f72af744d6369cc7b88a08c7e4bb
hank0 <hannes@q-ix.net>
2013-07-11 08:23:06 +0200

    fix

--- RadialTimeline/src/sketchBundlingEdges.java
+++ RadialTimeline/src/sketchBundlingEdges.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 import processing.core.PApplet;
 
-public class sketchBundlingEdges{
+public class sketchBundlingEdges {
 
 	PApplet pa;
 	public int width = 650;
@@ -15,7 +15,7 @@ public class sketchBundlingEdges{
 	ArrayList<bundlingEdges> allDiagrams = new ArrayList<bundlingEdges>();
 
 	// constructor
-	
+
 	sketchBundlingEdges(PApplet pa, int width, int height, int px, int py,
 			String[] persons, boolean matrix) {
 		this.pa = pa;
@@ -36,7 +36,7 @@ public class sketchBundlingEdges{
 			rtrn[1] = height / 2;
 			rtrn[2] = height / 2;
 			rtrn[3] = (height - 200) / 2;
-			
+
 		} else {
 			for (int j = 0; j < 2; j++) {
 				rtrn[1] = height / 4 - 10;
@@ -62,19 +62,19 @@ public class sketchBundlingEdges{
 		pa.strokeCap(PApplet.SQUARE);
 
 		ammountOfPersons = 0;
-		
-		for (int i = 0; i < persons.length; i++) {	
+
+		for (int i = 0; i < persons.length; i++) {
 			if (persons[i] != null) {
 				ammountOfPersons = ammountOfPersons + 1;
 			}
 		}
-		
+
 		float[] positions = computeDiagrammPositions();
-		
-		if(matrix){
+
+		if (matrix) {
 			String[] helperArray = new String[1];
 			for (int i = 0; i < ammountOfPersons; i++) {
-				
+
 				helperArray[0] = persons[i];
 				if (persons[i] != null) {
 					allDiagrams.add(new bundlingEdges(helperArray,
@@ -84,30 +84,28 @@ public class sketchBundlingEdges{
 				}
 
 			}
-		}else{
+		} else {
 			int length = 0;
-			for(int i=0;i<persons.length;i++){
-				if(persons[i]!= null){
+			for (int i = 0; i < persons.length; i++) {
+				if (persons[i] != null) {
 					length = length + 1;
 				}
 			}
 			String[] helperArray = new String[length];
 
 			int index = 0;
-			for(int i=0;i<persons.length;i++){
-				if(persons[i]!= null){
+			for (int i = 0; i < persons.length; i++) {
+				if (persons[i] != null) {
 					helperArray[index] = persons[i];
-					index = index+1;
+					index = index + 1;
 				}
 			}
 
-				allDiagrams.add(new bundlingEdges(helperArray,
-						height/2, height/2,
-						(height - 200) / 2, pa));
-				allDiagrams.get(0).handleDate();		
+			allDiagrams.add(new bundlingEdges(helperArray, height / 2,
+					height / 2, (height - 200) / 2, pa));
+			allDiagrams.get(0).handleDate();
 		}
-		
-		
+
 	}
 
 	public void draw() {
@@ -119,20 +117,19 @@ public class sketchBundlingEdges{
 
 			bundlingEdges b = allDiagrams.get(i);
 
-			b.setCntrX(positions[1 + 3 * i]+px);
-			b.setCntrY(positions[2 + 3 * i]+py);
+			b.setCntrX(positions[1 + 3 * i] + px);
+			b.setCntrY(positions[2 + 3 * i] + py);
 			b.setRadius(positions[3 + 3 * i]);
 
 			b.resetEverything();
 			b.drawBeziers = true;
 
 			b.drawArcsnArrows();
-			
+
 			b.drawContentArround();
-			if(matrix){
-				if(persons[i]!=null){
-					b.drawLabels(persons[i]);
-				}
+			if (matrix) {
+				b.drawLabels(persons[i]);
+
 			}
 		}
 	}
